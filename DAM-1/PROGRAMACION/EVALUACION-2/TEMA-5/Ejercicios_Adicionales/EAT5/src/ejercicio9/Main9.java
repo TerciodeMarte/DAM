b02498e18438404155bc917f5cc60eb694900c9b
Albano Díez de Paulino <albanodiezp@gmail.com>
2023-02-23 18:59:52 +0100

    Aprendizaje 23/02/2023

--- DAM-1/PROGRAMACION/EVALUACION-2/TEMA-5/Ejercicios_Adicionales/EAT5/src/ejercicio9/Main9.java
+++ DAM-1/PROGRAMACION/EVALUACION-2/TEMA-5/Ejercicios_Adicionales/EAT5/src/ejercicio9/Main9.java
@@ -10,10 +10,8 @@ public class Main9 {
 
     public static void main(String[] args) {
         Invierno i1=new Invierno(3);
-        double i2[]=new double[]{1,2,3,4,5,6};
         System.out.println("La temperatura media es: "+ i1.temp_media());
-        System.out.println("Las temperaturas en Fahrenheit son: " + Arrays.toString(i1.fahrenheit()));
-        System.out.println("El array que me has pasado es: "+i1.mostrar(i2));
+        System.out.println("Las temperaturas en Fahrenheit son: "+i1.mostrar(i1.fahrenheit()));
     }
 
 }
