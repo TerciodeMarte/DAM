df51f53471fb0758bd6ec4238c14c3a1d8d711bf
Albano Díez de Paulino <111312600+TerciodeMarte@users.noreply.github.com>
2023-03-15 19:07:14 +0100

    PROG-HOJA17-COR-EJ6

--- DAM-1/PROGRAMACION/EVALUACION-3/TEMA-6/HOJA17/hoja17/src/ejercicio6/Main6.java
+++ DAM-1/PROGRAMACION/EVALUACION-3/TEMA-6/HOJA17/hoja17/src/ejercicio6/Main6.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.ObjectInputStream;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
