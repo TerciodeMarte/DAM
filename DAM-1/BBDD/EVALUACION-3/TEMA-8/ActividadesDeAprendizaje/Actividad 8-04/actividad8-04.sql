2bea68597ad9c1fc512eb9063d05806d510e1936
Albano Díez de Paulino <albanodiezp@gmail.com>
2023-05-19 15:59:00 +0200

    BBDD-Actividad8-04-Parte1

---
+++ DAM-1/BBDD/EVALUACION-3/TEMA-8/ActividadesDeAprendizaje/Actividad 8-04/actividad8-04.sql
@@ -0,0 +1,5 @@
+use datosorigen;
+
+/*Todos los procedimientos se realizarán sobre la base de datos datosorigen. En la tabla 
+personas debes añadir una columna provincia VARCHAR(30).*/
+alter table personas add column provincia varchar(30);
