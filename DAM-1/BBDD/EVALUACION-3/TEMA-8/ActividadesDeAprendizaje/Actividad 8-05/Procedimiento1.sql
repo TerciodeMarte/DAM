a766c9889b420a41e116fc6488359571bfd9ad79
Albano Díez de Paulino <albanodiezp@gmail.com>
2023-05-21 19:16:53 +0200

    BBDD-DarOrden

--- DAM-1/BBDD/EVALUACION-3/TEMA-8/ActividadesDeAprendizaje/Actividad 8-05/Procedimiento1.sql
+++ DAM-1/BBDD/EVALUACION-3/TEMA-8/ActividadesDeAprendizaje/Actividad 8-05/Procedimiento1.sql
@@ -1,12 +1,16 @@
-CREATE PROCEDURE `dia_semana` (IN dia int,OUT nombre varchar(15))
+/*1.- Realiza un procedimiento asigna_fecha_nac que modifica la fecha de nacimiento de 
+una persona de la tabla personas con una fecha generada de forma aleatoria.
+● El procedimiento recibe el número de persona a modificar y una fecha mínima 
+y una fecha máxima entre las que se va a generar la fecha aleatoria.
+● Una forma de obtener la fecha aleatoria es:
+o Obtener la diferencia de días (D) entre la fecha máxima y la fecha 
+mínima.
+o Generar un número aleatorio N entre 0 y D.
+o Obtener la fecha sumando a la fecha mínima N días. 
+● Si la fecha máxima no es mayor que la mínima, el procedimiento no hace nada.*/
+
+CREATE PROCEDURE `asigna_fecha_nac` (in numero int,in fechamin date,in fechamax date)
 BEGIN
-case dia
-when 1 then set nombre='Lunes';
-when 2 then set nombre='Martes';
-when 3 then set nombre='Miercoles';
-when 4 then set nombre='Jueves';
-when 5 then set nombre='Viernes';
-when 6 then set nombre='Sabado';
-when 7 then set nombre='Domingo';
-END case;
+declare diff int;
+
 END
