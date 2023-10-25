b03c08b9f7f4e4eea7b880b8c1d285c5a1c58a24
Albano Díez de Paulino <albanodiezp@gmail.com>
2023-10-25 20:32:08 +0200

    AD-T2-Tarea5

--- DAM-2/AD/EVALUACION-1/T2-AccesoBBDD/TAREAS/Tarea-5/Tarea5/src/main/java/com/cafeconpalito/Main/ConexionBBDD.java
+++ DAM-2/AD/EVALUACION-1/T2-AccesoBBDD/TAREAS/Tarea-5/Tarea5/src/main/java/com/cafeconpalito/Main/ConexionBBDD.java
@@ -10,64 +10,64 @@ import java.sql.*;
 
 /**
  * Clase para rgestionar la conexion a la BBDDD
+ *
  * @author CafeConPalito
  */
 public class ConexionBBDD {
-    
+
     //Datos necesarios para configurar la conexión a la base de datos
-    static Connection con;
+    private static ConexionBBDD instance = null;
+    private Connection con = null;
     static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver"; // esto por lo que entiendo utiliza el driver que bajamos (el jar)
-    static final String USER = "root"; 
+    static final String USER = "root";
     static final String PASS = "1234";
     static final String BD = "tienda"; // nombre de la base de datos
     //la direccion para conectar a la bbdd se le pasa como parametro "BD"
-    
+
     //IP PARA TRABAJAR EN EL SERVER
     //static final String IP = "192.168.24.16";
-
     //IP PARA TRABAJAR EN LOCAL
     //static final String IP = "127.0.0.1";
     static final String IP = "localhost";
-    
-    //Todos los parametros para conectar a la BBDD
-    static final String DB_URL = "jdbc:mysql://"+ IP +":3306/"+BD+"?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC"; 
-       
-    
-    /**
-     * Metodo para crear una conexion a la BBDD
-     * @return Objeto de la clase Connection
-     */
-    public Connection conectar (){
-        
-        try {
 
+    //Todos los parametros para conectar a la BBDD
+    static final String DB_URL = "jdbc:mysql://" + IP + ":3306/" + BD + "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+
+    private ConexionBBDD() {
+     
+        try {
             Class.forName(JDBC_DRIVER);
-            con = DriverManager.getConnection(DB_URL, USER, PASS);
-            /*if (con != null) {
-                System.out.println("conexion realizada");
-            }*/
-            
+            this.con = DriverManager.getConnection(DB_URL, USER,PASS);
         } catch (SQLException ex) {
-            System.err.println("problemas de conexión\n" + ex.toString());
-        } catch (ClassNotFoundException e) {
-            System.err.println(e.toString());
+            Logger.getLogger(ConexionBBDD.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ConexionBBDD.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+       
+    }
+
+    public static ConexionBBDD getInstance() {
+        if (instance == null) {
+            instance = new ConexionBBDD();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
         return con;
-        
     }
 
     /**
      * Metodo para desconectar la BBDD
      */
-    public void desconectar(){
-        
+    public void desconectar() {
+
         try {
             con.close();
         } catch (SQLException ex) {
             Logger.getLogger(ConexionBBDD.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
     }
-    
+
 }
--- DAM-2/AD/EVALUACION-1/T2-AccesoBBDD/TAREAS/Tarea-5/Tarea5/src/main/java/com/cafeconpalito/Main/Tarea5.java
+++ DAM-2/AD/EVALUACION-1/T2-AccesoBBDD/TAREAS/Tarea-5/Tarea5/src/main/java/com/cafeconpalito/Main/Tarea5.java
@@ -11,41 +11,339 @@ import java.sql.SQLException;
  */
 public class Tarea5 {
 
-    private static ConexionBBDD conBD = new ConexionBBDD();
-    private static Connection con = conBD.conectar();
+   
+    private static Connection con = ConexionBBDD.getInstance().getConnection();
 
     public static void main(String[] args) {
 
-        ej1(con);
+       // ej1();
+       ej2();
+       //ej3();
+       //ej4();
+       //ej5();
+       //ej6();
+       //ej7();
+       //ej8();
+       //ej9();
+        //ej10();
     }
     
-    //1. Lista el nombre de todos los productos que hay en la tabla producto.
-    private static void ej1(Connection con){
+  /**
+     * 1. Lista el nombre de todos los productos que hay en la tabla producto.
+     */
+    public static void ej1() {
+ 
+        System.out.println("\nListando Todos los nombres de productos ");
         PreparedStatement ps = null;
         ResultSet rs = null;
-
-        // se prepara la sentencia para la BBDD como un String
-        String selectLogUsuario = "select * from producto";
-
+ 
+        String select = "select nombre from producto";
+ 
         try {
-
-           
-            //se crear el Statement Con la conexion a la BBDD y el String
-            ps = con.prepareStatement(selectLogUsuario);
-            //se añade al select el nombre del usuario
-            //se ejecuta la sentencia y devuelve un resultado
+ 
+            ps = con.prepareStatement(select);
+ 
             rs = ps.executeQuery();
-
-            //Es necesario usar el Next Para pasar a la primera Linea de la busqueda
-            //una vez en la primera linea podemos obtener la informacion;
+ 
+            //si encuentra resultado del reto de ese jugador busca la info
             while (rs.next()) {
-               System.out.println(rs.getInt(1) + rs.getString(2)+ rs.getDouble(3)+ rs.getInt(4)); 
+ 
+                System.out.println(rs.getString(1));
             }
-            
-
-        } catch (SQLException e) {
-            //System.err.println("ERROR AL LEER Login Usuario");
-
+ 
+        } catch (NumberFormatException e) {
+            System.err.println("Error de conversion de numero");
+        } catch (SQLException ex) {
+            System.err.println("Error en el select");
         }
+ 
+    }
+ 
+    /**
+     * 2. Lista los nombres y los precios de todos los productos de la tabla
+     * producto.
+     */
+    public static void ej2() {
+ 
+        System.out.println("\nListando Nombre y Precio de Productos");
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+ 
+        String select = "select nombre,precio from producto";
+ 
+        try {
+ 
+            ps = con.prepareStatement(select);
+ 
+            rs = ps.executeQuery();
+ 
+            //si encuentra resultado del reto de ese jugador busca la info
+            while (rs.next()) {
+                System.out.println(rs.getString(1) + " " + rs.getString(2));
+            }
+ 
+        } catch (NumberFormatException e) {
+            System.err.println("Error de conversion de numero");
+        } catch (SQLException ex) {
+            System.err.println("Error en el select");
+        }
+ 
+    }
+ 
+    /**
+     * 3. Lista todas las columnas de la tabla producto.
+     */
+    public static void ej3() {
+ 
+        System.out.println("\nListando Todo de productos (Select *)");
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+ 
+        String select = "select * from producto";
+ 
+        try {
+ 
+            ps = con.prepareStatement(select);
+ 
+            rs = ps.executeQuery();
+ 
+            //si encuentra resultado del reto de ese jugador busca la info
+            while (rs.next()) {
+ 
+                System.out.println(rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3) + " " + rs.getString(4));
+            }
+ 
+        } catch (NumberFormatException e) {
+            System.err.println("Error de conversion de numero");
+        } catch (SQLException ex) {
+            System.err.println("Error en el select");
+        }
+ 
+    }
+ 
+    /**
+     * 4. Lista el nombre de los productos, el precio en euros y el precio en
+     * dólares estadounidenses (USD)
+     */
+    public static void ej4() {
+ 
+        System.out.println("\nLista el nombre de los productos, el precio en euros y el precio en dólares estadounidenses (USD)");
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+ 
+        String select = "select nombre ,precio ,precio*0.9451 from producto";
+ 
+        try {
+ 
+            ps = con.prepareStatement(select);
+ 
+            rs = ps.executeQuery();
+ 
+            //si encuentra resultado del reto de ese jugador busca la info
+            while (rs.next()) {
+ 
+                System.out.println(rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3));
+            }
+ 
+        } catch (NumberFormatException e) {
+            System.err.println("Error de conversion de numero");
+        } catch (SQLException ex) {
+            System.err.println("Error en el select");
+        }
+ 
+    }
+ 
+    /**
+     * 5. Lista el nombre de los productos, el precio en euros y el precio en
+     * dólares estadounidenses (USD). Utiliza los siguientes alias para las
+     * columnas: nombre de producto, euros, dólares
+     */
+    public static void ej5() {
+ 
+        System.out.println("\nLista el nombre de los productos, el precio en euros y el precio en dólares estadounidenses (USD)");
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+ 
+        String select = "select nombre as 'Nombre de Producto' ,precio as 'Euros' ,precio*0.9451 as 'Dolares' from producto";
+ 
+        try {
+ 
+            ps = con.prepareStatement(select);
+ 
+            rs = ps.executeQuery();
+ 
+            //si encuentra resultado del reto de ese jugador busca la info
+            while (rs.next()) {
+ 
+                System.out.println(rs.getString(1) + " " + rs.getString(2) + " " + rs.getString(3));
+            }
+ 
+        } catch (NumberFormatException e) {
+            System.err.println("Error de conversion de numero");
+        } catch (SQLException ex) {
+            System.err.println("Error en el select");
+        }
+ 
+    }
+ 
+    /**
+     * 6. Lista los nombres y los precios de todos los productos de la tabla
+     * producto, convirtiendo los nombres a mayúscula
+     */
+    public static void ej6() {
+ 
+        System.out.println("\nLista el nombre de los productos en Mayusculas, el precio");
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+ 
+        String select = "select UPPER(nombre),precio from producto";
+ 
+        try {
+ 
+            ps = con.prepareStatement(select);
+ 
+            rs = ps.executeQuery();
+ 
+            //si encuentra resultado del reto de ese jugador busca la info
+            while (rs.next()) {
+ 
+                System.out.println(rs.getString(1) + " " + rs.getString(2));
+            }
+ 
+        } catch (NumberFormatException e) {
+            System.err.println("Error de conversion de numero");
+        } catch (SQLException ex) {
+            System.err.println("Error en el select");
+        }
+ 
+    }
+ 
+    /**
+     * 7. Lista los nombres y los precios de todos los productos de la tabla
+     * producto, convirtiendo los nombres a minúscula
+     */
+    public static void ej7() {
+ 
+        System.out.println("\nLista el nombre de los productos en Minuscula, el precio");
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+ 
+        String select = "select LOWER(nombre),precio from producto";
+ 
+        try {
+ 
+            ps = con.prepareStatement(select);
+ 
+            rs = ps.executeQuery();
+ 
+            //si encuentra resultado del reto de ese jugador busca la info
+            while (rs.next()) {
+ 
+                System.out.println(rs.getString(1) + " " + rs.getString(2));
+            }
+ 
+        } catch (NumberFormatException e) {
+            System.err.println("Error de conversion de numero");
+        } catch (SQLException ex) {
+            System.err.println("Error en el select");
+        }
+ 
+    }
+ 
+    /**
+     * 8. Lista el nombre de todos los fabricantes en una columna, y en otra
+     * columna obtenga en mayúsculas los dos primeros caracteres del nombre del
+     * fabricante
+     */
+    public static void ej8() {
+ 
+        System.out.println("\nLista el nombre de los fabricantes, y los dos primeros caracteres del nombre en mayus");
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+ 
+        String select = "select nombre, upper(substring(nombre,1,2)) from fabricante";
+ 
+        try {
+ 
+            ps = con.prepareStatement(select);
+ 
+            rs = ps.executeQuery();
+ 
+            //si encuentra resultado del reto de ese jugador busca la info
+            while (rs.next()) {
+ 
+                System.out.println(rs.getString(1) + " " + rs.getString(2));
+            }
+ 
+        } catch (NumberFormatException e) {
+            System.err.println("Error de conversion de numero");
+        } catch (SQLException ex) {
+            System.err.println("Error en el select");
+        }
+ 
+    }
+ 
+    /**
+     * 9. Lista los nombres y los precios de todos los productos de la tabla producto,
+    *redondeando el valor del precio.
+     */
+    public static void ej9() {
+ 
+        System.out.println("\nLista el nombre de los productos , el precio redondeado");
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+ 
+        String select = "select nombre, round(precio,0) from producto";
+ 
+        try {
+ 
+            ps = con.prepareStatement(select);
+ 
+            rs = ps.executeQuery();
+ 
+            //si encuentra resultado del reto de ese jugador busca la info
+            while (rs.next()) {
+ 
+                System.out.println(rs.getString(1) + " " + rs.getString(2));
+            }
+ 
+        } catch (NumberFormatException e) {
+            System.err.println("Error de conversion de numero");
+        } catch (SQLException ex) {
+            System.err.println("Error en el select");
+        }
+ 
+    }
+
+    /**
+    * 10. Lista los nombres y los precios de todos los productos de la tabla producto,
+    *truncando el valor del precio para mostrarlo sin ninguna cifra decimal.
+     */
+    public static void ej10() {
+ 
+        System.out.println("\nLista el nombre de los productos , el precio sin decimal");
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+ 
+        String select = "select nombre, format(precio,0) from producto";
+ 
+        try {
+ 
+            ps = con.prepareStatement(select);
+ 
+            rs = ps.executeQuery();
+ 
+            //si encuentra resultado del reto de ese jugador busca la info
+            while (rs.next()) {
+ 
+                System.out.println(rs.getString(1) + " " + rs.getString(2));
+            }
+ 
+        } catch (NumberFormatException e) {
+            System.err.println("Error de conversion de numero");
+        } catch (SQLException ex) {
+            System.err.println("Error en el select");
+        }
+ 
     }
 }
