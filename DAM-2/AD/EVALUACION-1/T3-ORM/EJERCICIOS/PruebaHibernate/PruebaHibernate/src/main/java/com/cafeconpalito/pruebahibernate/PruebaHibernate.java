8ba908c402c15ccfeb29f2ccb74841f74e9f2d07
Albano Díez de Paulino <111312600+TerciodeMarte@users.noreply.github.com>
2023-11-13 20:28:26 +0100

    AD-T3-PruebaHibernate

---
+++ DAM-2/AD/EVALUACION-1/T3-ORM/EJERCICIOS/PruebaHibernate/PruebaHibernate/src/main/java/com/cafeconpalito/pruebahibernate/PruebaHibernate.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.cafeconpalito.pruebahibernate;
+
+/**
+ *
+ * @author carra
+ */
+public class PruebaHibernate {
+
+    public static void main(String[] args) {
+        System.out.println("Hello There");
+    }
+}
