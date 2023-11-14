b03a9076c016e10ed7f9fbf0d468d2c1c4b9cf0f
Albano Díez de Paulino <albanodiezp@gmail.com>
2023-11-14 17:23:16 +0100

    PSP-T2-Hoja6

--- DAM-2/PSP/EVALUACION-1/T2- Programacion Multihilo/EJERCICIOS/HOJA6/Hoja6/src/main/java/com/cafeconpalito/hoja6/Pista.java
+++ DAM-2/PSP/EVALUACION-1/T2- Programacion Multihilo/EJERCICIOS/HOJA6/Hoja6/src/main/java/com/cafeconpalito/hoja6/Pista.java
@@ -11,18 +11,11 @@ public class Pista {
 
     private int corredores;
     private int encarrera;
-    private boolean relevo;
     private boolean enmarcas;
-
-    public void setRelevo(boolean relevo) {
-        this.relevo = relevo;
-    }
-    
 
     public Pista() {
         this.corredores = 0;
-        this.relevo = false;
-        this.enmarcas=false;
+        this.enmarcas = false;
     }
 
     public synchronized void marcas() {
@@ -35,35 +28,27 @@ public class Pista {
             }
         }
         if (!enmarcas) {
-            encarrera=corredores+1;
+            encarrera = corredores + 1;
             System.out.println("Todos en sus marcas");
-            enmarcas=true;
+            enmarcas = true;
         }
     }
-    
-    public synchronized void correr(){
-        while (relevo) {
-            try {
-                wait();
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Pista.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
+
+    public synchronized void correr() {
+
         try {
-            relevo= true;
-            Thread.sleep((long) (Math.random()*2000));
+
+            Thread.sleep((long) (Math.random() * 2000));
             encarrera--;
-            if (encarrera!=0) {
+            if (encarrera != 0) {
                 System.out.println("Pasando el testigo");
             }
-            
-            relevo=false;
-            
+
             notify();
- 
+
         } catch (InterruptedException ex) {
             Logger.getLogger(Pista.class.getName()).log(Level.SEVERE, null, ex);
-            
+
         }
     }
 
