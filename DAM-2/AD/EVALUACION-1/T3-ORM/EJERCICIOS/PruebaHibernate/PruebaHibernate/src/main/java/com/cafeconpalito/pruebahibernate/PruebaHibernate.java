dd33a4ca501d584f55ac18e6078acf5ea0f21408
Albano Díez de Paulino <albanodiezp@gmail.com>
2023-11-20 21:00:40 +0100

    AD-T3-PruebaHibernate

--- DAM-2/AD/EVALUACION-1/T3-ORM/EJERCICIOS/PruebaHibernate/PruebaHibernate/src/main/java/com/cafeconpalito/pruebahibernate/PruebaHibernate.java
+++ DAM-2/AD/EVALUACION-1/T3-ORM/EJERCICIOS/PruebaHibernate/PruebaHibernate/src/main/java/com/cafeconpalito/pruebahibernate/PruebaHibernate.java
@@ -4,6 +4,12 @@
 
 package com.cafeconpalito.pruebahibernate;
 
+import java.util.ArrayList;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
 /**
  *
  * @author carra
@@ -12,5 +18,21 @@ public class PruebaHibernate {
 
     public static void main(String[] args) {
         System.out.println("Hello There");
+        
+        //Creo la conexion
+        EntityManagerFactory emf= Persistence.createEntityManagerFactory("com.cafeconpalito_PruebaHibernate_jar_1.0PU");
+        EntityManager em =emf.createEntityManager();
+        
+        Query c1 = em.createNamedQuery("Cliente.findAll");
+        
+        ArrayList<Cliente> al= (ArrayList<Cliente>) c1.getResultList();
+        for (Cliente cliente : al) {
+            System.out.println(cliente.getNombre());
+        }
+        
+        //Cierro la conexion
+        em.close();
+        emf.close();
+        
     }
 }
