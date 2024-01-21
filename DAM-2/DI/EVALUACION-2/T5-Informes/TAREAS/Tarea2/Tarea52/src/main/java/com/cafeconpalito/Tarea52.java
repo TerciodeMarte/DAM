0acc18bd37a3c968a58e8fb157a7f90d31c2f312
Albano Díez de Paulino <adiezd07@educantabria.es>
2024-01-21 18:22:43 +0100

    DI-T5-Tarea2

---
+++ DAM-2/DI/EVALUACION-2/T5-Informes/TAREAS/Tarea2/Tarea52/src/main/java/com/cafeconpalito/Tarea52.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.cafeconpalito;
+
+import java.sql.Connection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+
+/**
+ *
+ * @author TerciodeMarte
+ */
+public class Tarea52 {
+
+    private static Connection con = ConexionBBDD.getInstance().getConnection();
+
+    public static void main(String[] args) {
+        Map hm = new HashMap();
+
+        JasperPrint print;
+        try {
+            print = JasperFillManager.fillReport("Tarea52.jasper", hm, con);
+            JasperExportManager.exportReportToPdfFile(print, "informe.pdf");
+        } catch (JRException ex) {
+            Logger.getLogger(Tarea52.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
